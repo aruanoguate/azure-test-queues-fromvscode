0c180276a8fbc8c1f6e48e62b8f22d33a7dd0a9d
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-03-07 00:21:13 -0600

    Peek the queue

--- storage-account-management.sh
+++ storage-account-management.sh
@@ -3,3 +3,7 @@ az storage account create --name mystoragemar72021 -g learn-8b529a3b-30e9-4a94-a
 
 # Get Connection String
 az storage account show-connection-string --name mystoragemar72021 --resource-group learn-8b529a3b-30e9-4a94-a30e-a6b89ee81906
+
+# Peek the messages in the queue
+AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=mystoragemar72021;AccountKey=FTLX2IK7oaophgq83voHtzMQaJsSeAVB+ThczBImz3OIwT7zbG/+utrisccfbpmSpAZzf6Zch/zcqtB6+iwxbA=="
+az storage message peek --queue-name newsqueue --connection-string $AZURE_STORAGE_CONNECTION_STRING
