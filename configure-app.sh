b4d42f3a05c547e64fade340ba9b81b875238fe7
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-03-06 23:44:21 -0600

    Commands used to manage the application

---
+++ configure-app.sh
@@ -0,0 +1,11 @@
+# Create new project
+dotnet new console -n QueueApp
+
+# Add a NuGet package
+dotnet add package WindowsAzure.Storage
+
+# Build the solution
+dotnet build
+
+# Execute the project
+dotnet run
