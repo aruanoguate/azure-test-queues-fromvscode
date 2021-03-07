e68cc8ba2679db37dfacd18cd6924be293989dea
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-03-07 00:33:05 -0600

    Adding reader method to recover messages from the Queue

--- Program.cs
+++ Program.cs
@@ -17,15 +17,24 @@ namespace QueueApp
                 await SendArticleAsync(value);
                 Console.WriteLine($"Sent: {value}");
             }
+            else
+            {
+                string value = await ReceiveArticleAsync();
+                Console.WriteLine($"Received {value}");
+            }
         }
 
-        static async Task SendArticleAsync(string newsMessage)
+        static CloudQueue GetQueue()
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
 
             CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            return queueClient.GetQueueReference("newsqueue");
+        }
 
-            CloudQueue queue = queueClient.GetQueueReference("newsqueue");
+        static async Task SendArticleAsync(string newsMessage)
+        {
+            CloudQueue queue = GetQueue();
             bool createdQueue = await queue.CreateIfNotExistsAsync();
             if (createdQueue)
             {
@@ -34,6 +43,25 @@ namespace QueueApp
 
             CloudQueueMessage articleMessage = new CloudQueueMessage(newsMessage);
             await queue.AddMessageAsync(articleMessage);
+        }
+
+        static async Task<string> ReceiveArticleAsync()
+        {
+            CloudQueue queue = GetQueue();
+
+            bool exists = await queue.ExistsAsync();
+            if (exists)
+            {
+                CloudQueueMessage retrievedArticle = await queue.GetMessageAsync();
+                if (retrievedArticle != null)
+                {
+                    string newsMessage = retrievedArticle.AsString;
+                    await queue.DeleteMessageAsync(retrievedArticle);
+                    return newsMessage;
+                }
+            }
+
+            return "<queue empty or not created>";
         }
     }
 }
